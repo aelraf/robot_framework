effd218c3ce07b5ab187e94036f3f8a5cf6bff06
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-03-15 15:14:59 +0100

    NewSeleniumLibrary - metody get_current_appium_driver i get_email

--- Bitel55/Libraries/NewSeleniumLibrary.py
+++ Bitel55/Libraries/NewSeleniumLibrary.py
@@ -1,14 +1,19 @@
+from robot.libraries.BuiltIn import BuiltIn
 from selenium.webdriver.common.by import By
 
 
 class NewSeleniumLibrary:
-    EMAIL = None
+    EMAIL = 'e.mail'
 
     @property
     def get_current_web_driver(self):
-        selenium_library = BuiltIn().get_library_instance()
+        selenium_library = BuiltIn().get_library_instance('Selenium library')
         return selenium_library.driver
 
     @property
-    def get_current_driver(self):
-        pass
+    def get_current_appium_driver(self):
+        appium_driver = BuiltIn().get_library_instance('AppiumLibrary')._current_application()
+        return appium_driver
+
+    def get_email(self):
+        return self.get_current_web_driver.find_element(By.CSS_SELECTOR, self.EMAIL).text
