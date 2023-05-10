3e3b85f181473c4c6b8094951471658df294e3ce
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2023-05-10 20:08:25 +0200

    [EKW] - firs look of skeleton and methods

---
+++ EKW/ekw.py
@@ -0,0 +1,22 @@
+#
+
+import requests
+
+
+def count_a_control_digit(number):
+    digit = 0
+    return digit
+
+
+def read_from_rest():
+    response = []
+    return response
+
+
+def analise_of_response(response):
+    data = []
+    return data
+
+
+if __name__ == "__main__":
+    pass
